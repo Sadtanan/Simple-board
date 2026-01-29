7ce702e7a1c92c44d9e5796f074a4bd0de8f8850
Sadtanan <youhtzaa@hotmail.com>
2026-01-29 18:10:20 +0700

    add create page and validation(empty)

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/create", methods=["GET", "POST"])
+def create():
+    if request.method == "POST":
+        title = request.form.get("title")
+        content = request.form.get("content")
+
+        # ยัง
+        print(title, content)
+
+    return render_template("create.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
